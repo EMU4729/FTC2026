b041f8a9b4f34a1f27a4822a960b8406ec144dc3
Ricky L <457297106@student.sbhs.nsw.edu.au>
2025-09-10 18:31:39 +1000

    Created Subsystems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/subsystems/IndexSubsystem.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.lib.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class IndexSubsystem extends SubsystemBase{
+    public IndexSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/subsystems/IntakeSubsystem.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.lib.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class IntakeSubsystem extends SubsystemBase{
+    public IntakeSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+
+
+    }
+}
