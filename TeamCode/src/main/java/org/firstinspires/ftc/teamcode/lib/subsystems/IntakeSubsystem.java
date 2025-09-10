70be1985f06b253df1b10474fde4daacc600aae7
Ricky L <457297106@student.sbhs.nsw.edu.au>
2025-09-10 19:07:02 +1000

    Added a method in Intake subsystem to rotate a motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/subsystems/IntakeSubsystem.java
@@ -1,12 +1,24 @@
 package org.firstinspires.ftc.teamcode.lib.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-public class IntakeSubsystem extends SubsystemBase{
+public class IntakeSubsystem extends SubsystemBase {
+    private final DcMotor spinMotor;
+    private final Telemetry telemetry;
+
     public IntakeSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        spinMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        this.telemetry = telemetry;
+    }
 
-
+    /**
+     * Spin the intake motor for intake
+     * @param spin Motor power in [-1, 1]
+     */
+    public void rotateSpinMotor(double spin) {
+        spinMotor.setPower(spin);
     }
 }
