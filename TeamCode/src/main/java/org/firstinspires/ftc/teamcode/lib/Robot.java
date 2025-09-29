9694fb03649f6a2b4b82a9e401bf8297658e85dd
Neel Sharma <nsdj.sharma@gmail.com>
2025-09-29 17:15:07 +1000

    Remove the mode-specific methods in the Robot class
    All this logic should be in the opmode files anyways

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Robot.java
@@ -50,29 +50,4 @@ public class Robot {
         shooter.periodic();
         telemetry.addData("Code Version", VERSION_NUMBER);
     }
-
-    /**
-     * This method should be called only once when a teleop opmode starts.
-     */
-    public void teleopInit() {
-        // If we ever need any initialization code, this is where to keep it.
-    }
-
-    /**
-     * This method should be called once every robot loop in teleop opmodes only.
-     * <p>
-     * This is a good place to keep all gamepad button bindings.
-     */
-    public void teleopPeriodic() {
-        // drive bindings
-        drive.driveRobotRelative(-gamepad2.left_stick_y, gamepad2.left_stick_x, -gamepad2.right_stick_x);
-    }
-
-    /**
-     * This method should be called regularly in the teleop testing opmode.
-     * <p>
-     * This is a good place to keep any testing code that should not be used in competition.
-     */
-    public void teleopTestPeriodic() {
-    }
 }
