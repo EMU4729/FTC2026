b73968b80043a043cff0ca9110bb6da230748937
zodaz0 <lucaforzisi@outlook.com>
2025-10-01 19:02:17 +1000

    seperated the lift and shoot subsystem, removed pto stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/subsystems/LiftSubsystem.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.lib.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class LiftSubsystem extends SubsystemBase {
+    private static final double LIFT_DISTANCE_PER_TICK = 1;
+    private static final double LIFT_PID_P = 0;
+
+    private final DcMotorEx leftMotor;
+    private final DcMotorEx rightMotor;
+
+    public LiftSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        leftMotor = hardwareMap.get(DcMotorEx.class, "liftL");
+        rightMotor = hardwareMap.get(DcMotorEx.class, "liftR");
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftMotor.setPositionPIDFCoefficients(LIFT_PID_P);
+        rightMotor.setPositionPIDFCoefficients(LIFT_PID_P);
+    }
+
+    /**
+     * sets left right motor target position to given distance
+     *
+     * @param pos the distance in m
+     */
+    private void liftGoTo(double pos) {
+        int _pos = (int) (pos / LIFT_DISTANCE_PER_TICK);
+        leftMotor.setTargetPosition(_pos);
+        rightMotor.setTargetPosition(_pos);
+    }
+}
+
+
