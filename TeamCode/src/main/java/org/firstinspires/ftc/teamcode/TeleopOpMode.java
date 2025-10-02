26698a42975a2c40372e9817cec4747b4ee11dab
Neel Sharma <nsdj.sharma@gmail.com>
2025-10-02 22:22:36 +1000

    yolo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -4,26 +4,32 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.lib.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.lib.subsystems.IndexSubsystem;
+import org.firstinspires.ftc.teamcode.lib.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.lib.subsystems.LiftSubsystem;
+import org.firstinspires.ftc.teamcode.lib.subsystems.ShooterSubsystem;
 
 @TeleOp(name = "TeleOp")
 public class TeleopOpMode extends OpMode {
     DriveSubsystem drive;
     LiftSubsystem lift;
+    IntakeSubsystem intake;
+    IndexSubsystem index;
+    ShooterSubsystem shooter;
 
     @Override
     public void init() {
         drive = new DriveSubsystem(hardwareMap, telemetry);
         lift = new LiftSubsystem(hardwareMap, telemetry);
-    }
-
-    @Override
-    public void start() {
+        intake = new IntakeSubsystem(hardwareMap, telemetry);
+        index = new IndexSubsystem(hardwareMap, telemetry);
+        shooter = new ShooterSubsystem(hardwareMap, telemetry);
     }
 
     @Override
     public void loop() {
         drive.driveRobotRelative(-gamepad2.left_stick_y, gamepad2.left_stick_x, -gamepad2.right_stick_x);
+
         if (gamepad1.dpad_up) {
             lift.setPower(0.5);
         } else if (gamepad1.dpad_down) {
@@ -31,8 +37,35 @@ public class TeleopOpMode extends OpMode {
         } else {
             lift.setPower(0);
         }
+
+        if (gamepad1.left_trigger > 0.5) {
+            intake.setPower(1);
+        } else {
+            intake.setPower(0);
+        }
+
+        if (gamepad1.dpad_right) {
+            index.setMode(IndexSubsystem.Mode.CLOCKWISE);
+        } else if (gamepad1.dpad_left) {
+            index.setMode(IndexSubsystem.Mode.ANTICLOCKWISE);
+        } else {
+            index.setMode(IndexSubsystem.Mode.IDLE);
+        }
+
+        if (gamepad1.right_trigger > 0.5) {
+            // NOTE: if the shooter is acting weird, the issue is probably in these numbers
+            shooter.pop();
+            shooter.shoot(0.8, 0.5);
+        } else {
+            shooter.shoot(0, 0.5);
+            shooter.unpop();
+        }
+
         drive.periodic();
         lift.periodic();
+        intake.periodic();
+        index.periodic();
+        shooter.periodic();
         telemetry.update();
     }
 }
