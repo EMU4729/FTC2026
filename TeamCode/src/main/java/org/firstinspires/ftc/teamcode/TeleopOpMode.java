abf29113bda139f13428d5b985fd7bb7be258f16
Neel Sharma <nsdj.sharma@gmail.com>
2025-10-02 21:54:05 +1000

    Make TeleopOpMode only rely on drive subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -3,15 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.lib.Robot;
+import org.firstinspires.ftc.teamcode.lib.subsystems.DriveSubsystem;
 
 @TeleOp(name = "TeleOp")
 public class TeleopOpMode extends OpMode {
-    Robot robot;
+    DriveSubsystem drive;
 
     @Override
     public void init() {
-        robot = new Robot(hardwareMap, telemetry, gamepad1, gamepad2);
+        drive = new DriveSubsystem(hardwareMap, telemetry);
     }
 
     @Override
@@ -20,8 +20,8 @@ public class TeleopOpMode extends OpMode {
 
     @Override
     public void loop() {
-        robot.drive.driveRobotRelative(-gamepad2.left_stick_y, gamepad2.left_stick_x, -gamepad2.right_stick_x);
-        robot.periodic();
+        drive.driveRobotRelative(-gamepad2.left_stick_y, gamepad2.left_stick_x, -gamepad2.right_stick_x);
+        drive.periodic();
         telemetry.update();
     }
 }
