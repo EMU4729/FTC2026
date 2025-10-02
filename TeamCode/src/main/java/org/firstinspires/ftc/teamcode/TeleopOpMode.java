4dede2b45418d2579bead6de4c92423a4bfed1af
Neel Sharma <nsdj.sharma@gmail.com>
2025-10-02 22:04:31 +1000

    Temporarily make lift motors manually operated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -4,14 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.lib.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.lib.subsystems.LiftSubsystem;
 
 @TeleOp(name = "TeleOp")
 public class TeleopOpMode extends OpMode {
     DriveSubsystem drive;
+    LiftSubsystem lift;
 
     @Override
     public void init() {
         drive = new DriveSubsystem(hardwareMap, telemetry);
+        lift = new LiftSubsystem(hardwareMap, telemetry);
     }
 
     @Override
@@ -21,7 +24,15 @@ public class TeleopOpMode extends OpMode {
     @Override
     public void loop() {
         drive.driveRobotRelative(-gamepad2.left_stick_y, gamepad2.left_stick_x, -gamepad2.right_stick_x);
+        if (gamepad1.dpad_up) {
+            lift.setPower(0.5);
+        } else if (gamepad1.dpad_down) {
+            lift.setPower(-0.5);
+        } else {
+            lift.setPower(0);
+        }
         drive.periodic();
+        lift.periodic();
         telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/subsystems/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/subsystems/LiftSubsystem.java
@@ -1,14 +1,13 @@
 package org.firstinspires.ftc.teamcode.lib.subsystems;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class LiftSubsystem extends SubsystemBase {
-    private static final double LIFT_DISTANCE_PER_TICK = 1;
-    private static final double LIFT_PID_P = 0;
+    private static final double LIFT_DISTANCE_PER_TICK = 1 / 537.7;
+    private static final double LIFT_PID_P = 1;
 
     private final DcMotorEx leftMotor;
     private final DcMotorEx rightMotor;
@@ -16,12 +15,14 @@ public class LiftSubsystem extends SubsystemBase {
     public LiftSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
         leftMotor = hardwareMap.get(DcMotorEx.class, "liftL");
         rightMotor = hardwareMap.get(DcMotorEx.class, "liftR");
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftMotor.setPositionPIDFCoefficients(LIFT_PID_P);
-        rightMotor.setPositionPIDFCoefficients(LIFT_PID_P);
+//        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        leftMotor.setTargetPosition(0);
+//        rightMotor.setTargetPosition(0);
+//        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        leftMotor.setPositionPIDFCoefficients(LIFT_PID_P);
+//        rightMotor.setPositionPIDFCoefficients(LIFT_PID_P);
     }
 
     /**
@@ -33,6 +34,16 @@ public class LiftSubsystem extends SubsystemBase {
         int _pos = (int) (pos / LIFT_DISTANCE_PER_TICK);
         leftMotor.setTargetPosition(_pos);
         rightMotor.setTargetPosition(_pos);
+    }
+
+    /**
+     * Directly sets the power of the lift motors.
+     *
+     * @param power The power in the range [-1, 1]
+     */
+    public void setPower(double power) {
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
     }
 }
 
