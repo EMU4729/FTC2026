cac9e86a7cc8816e7248a1197a5fe55e1b319d00
Pigeon Nation <134137685+pigeon-nation@users.noreply.github.com>
2025-11-05 19:34:23 +1100

    enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopOpMode.java
@@ -19,7 +19,11 @@ public class TeleopOpMode extends OpMode {
     ShooterSubsystem shooter;
     LEDSubsystem led;
 
-    double currentArc = 0.8;
+    private enum ShooterStates {
+        IDLE,
+        PREPING,
+        SHOOTING
+    }
 
     @Override
     public void init() {
@@ -35,43 +39,7 @@ public class TeleopOpMode extends OpMode {
     public void loop() {
         drive.driveRobotRelative(-gamepad2.left_stick_y, gamepad2.left_stick_x, -gamepad2.right_stick_x);
 
-        if (gamepad1.dpad_up) {
-            lift.setPower(0.5);
-        } else if (gamepad1.dpad_down) {
-            lift.setPower(-0.5);
-        } else {
-            lift.setPower(0);
-        }
 
-        if (gamepad1.left_trigger > 0.5) {
-            intake.setPower(1);
-        } else {
-            intake.setPower(0);
-        }
-
-        if (gamepad1.dpad_right) {
-            index.setMode(IndexSubsystem.Mode.CLOCKWISE);
-        } else if (gamepad1.dpad_left) {
-            index.setMode(IndexSubsystem.Mode.ANTICLOCKWISE);
-        } else {
-            index.setMode(IndexSubsystem.Mode.IDLE);
-        }
-
-        if (gamepad1.right_bumper) {
-            currentArc = Math.min(currentArc + 0.05, 1);
-        } else if (gamepad1.left_bumper) {
-            currentArc = Math.max(currentArc - 0.05, 0);
-        }
-
-        if (gamepad1.right_trigger > 0.5) {
-            // NOTE: if the shooter is acting weird, the issue is probably in these numbers
-            shooter.pop();
-            shooter.setSpeed(1);
-        } else {
-            shooter.setSpeed(0);
-            shooter.unpop();
-        }
-        shooter.setArc(currentArc);
 
         drive.periodic();
         lift.periodic();
