7f9a631547be7de175e11d0b08de5bcfd12ffe12
qweggry0 <jason.du986@gmail.com>
2025-11-19 18:00:57 +1100

    added backup autoOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackupAutoOpMode.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.lib.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.lib.subsystems.IndexSubsystem;
+import org.firstinspires.ftc.teamcode.lib.subsystems.ShooterSubsystem;
+
+
+public class BackupAutoOpMode extends OpMode {
+    private DriveSubsystem drive;
+    private final ElapsedTime timer = new ElapsedTime();
+
+    @Override
+    public void init() {
+        drive = new DriveSubsystem(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void start() {
+        timer.reset();
+        while (timer.time() < 1)
+            drive.driveRobotRelative(1, 0, 0);
+        drive.stop();
+    }
+
+    @Override
+    public void loop() {
+    }
+}
