b254e5c984637aec6c54e206d04ef764ba42ab4e
Neel Sharma <nsdj.sharma@gmail.com>
2025-12-04 19:26:30 +1100

    add lift test op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTestOpMode.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.lib.subsystems.LiftSubsystem;
+
+public class LiftTestOpMode extends OpMode {
+    private LiftSubsystem lift;
+    private double lockPosition = 1;
+
+    public void init() {
+        lift = new LiftSubsystem(hardwareMap, telemetry);
+    }
+
+    public void loop() {
+        if (gamepad1.dpad_left) {
+            lockPosition = Math.max(lockPosition - 0.01, 0);
+        } else if (gamepad1.dpad_right) {
+            lockPosition = Math.min(lockPosition + 0.01, 1);
+        }
+        lift.setLockPosition(lockPosition);
+
+        if (gamepad1.left_bumper) {
+            lift.setLeftPower(1);
+        } else {
+            lift.setLeftPower(0);
+        }
+
+        if (gamepad1.right_bumper) {
+            lift.setRightPower(1);
+        } else {
+            lift.setRightPower(0);
+        }
+    }
+}
